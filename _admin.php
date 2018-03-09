1ce0ce7f5144226ae5bbd0e56657014a0537daad
franck <carnet.franck.paul@gmail.com>
2018-03-09 15:10:20 +0100

    Various code formatting

--- _admin.php
+++ _admin.php
@@ -1,122 +1,123 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Media Navigator, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief navmedia, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_CONTEXT_ADMIN')) { return; }
+if (!defined('DC_CONTEXT_ADMIN')) {return;}
 
 // dead but useful code, in order to have translations
-__('Media Navigator').__('Navigate between media in folder');
+__('Media Navigator') . __('Navigate between media in folder');
 
-$core->addBehavior('adminMediaItemForm',array('adminNavMedia','adminMediaItemForm'));
+$core->addBehavior('adminMediaItemForm', array('adminNavMedia', 'adminMediaItemForm'));
 
 class adminNavMedia
 {
-	/**
-	adminMediaItemForm behavior
-	
-	@param	file	<b>fileItem</b>	File item
-	*/
-	public static function adminMediaItemForm($file)
-	{
-		global $core;
+    /**
+    adminMediaItemForm behavior
 
-		// Récupération des infos nécessaires à la construction des liens de navigation
+    @param    file    <b>fileItem</b>    File item
+     */
+    public static function adminMediaItemForm($file)
+    {
+        global $core;
 
-		// identificateur du billet en cours d'édition
-		$post_id = !empty($_GET['post_id']) ? (integer) $_GET['post_id'] : null;
+        // Récupération des infos nécessaires à la construction des liens de navigation
 
-		// Indicateur d'affichage popup de la page détail du média
-		$popup = (integer) !empty($_GET['popup']);
+        // identificateur du billet en cours d'édition
+        $post_id = !empty($_GET['post_id']) ? (integer) $_GET['post_id'] : null;
 
-		// Paramètres supplémentaires pour les URLs
-		$opt = '&amp;popup='.$popup.'&amp;post_id='.$post_id;
+        // Indicateur d'affichage popup de la page détail du média
+        $popup = (integer) !empty($_GET['popup']);
 
-		// Identificateur du média
-		$id = !empty($_REQUEST['id']) ? (integer) $_REQUEST['id'] : '';
+        // Paramètres supplémentaires pour les URLs
+        $opt = '&amp;popup=' . $popup . '&amp;post_id=' . $post_id;
 
-		if (dirname($file->relname) != "") {
+        // Identificateur du média
+        $id = !empty($_REQUEST['id']) ? (integer) $_REQUEST['id'] : '';
 
-			// Construction de l'objet de parcours du répertoire dans lequel se trouve le média courant
-			$mp_media = new dcMedia($core);
-			// Changement du répertoire courant
-			$mp_media->chdir(dirname($file->relname));
-			// Récupération du contenu du répertoire
-			$mp_media->getDir();
-			$mp_dir =& $mp_media->dir;
+        if (dirname($file->relname) != "") {
 
-			// Récupération de la liste des fichiers uniquement (les sous-répertoires sont exclus)
-			$mp_items = array_values(array_merge($mp_dir['files']));
-			if (count($mp_items) > 1) {
+            // Construction de l'objet de parcours du répertoire dans lequel se trouve le média courant
+            $mp_media = new dcMedia($core);
+            // Changement du répertoire courant
+            $mp_media->chdir(dirname($file->relname));
+            // Récupération du contenu du répertoire
+            $mp_media->getDir();
+            $mp_dir = &$mp_media->dir;
 
-				// On a plus d'un fichier dans le répertoire
-				// Reprise de la présentation utilisée dans la gestion des médias.
+            // Récupération de la liste des fichiers uniquement (les sous-répertoires sont exclus)
+            $mp_items = array_values(array_merge($mp_dir['files']));
+            if (count($mp_items) > 1) {
 
-//				echo '<h3>'.__('Navigation').'</h3>';
-				echo '<div class="media-list"><div class="files-group">';
+                // On a plus d'un fichier dans le répertoire
+                // Reprise de la présentation utilisée dans la gestion des médias.
 
-				for ($mp_i = 0; $mp_i < count($mp_items); $mp_i++) {
-					if ($mp_items[$mp_i]->media_id == $file->media_id) {
-						// On a trouvé le média courant dans la liste
+//                echo '<h3>'.__('Navigation').'</h3>';
+                echo '<div class="media-list"><div class="files-group">';
 
-						// Média précédent
-						echo '<div class="media-item media-col-0">'.'<h4>'.__('Previous media:').'</h4>'.
-							($mp_i > 0 ? self::displayMediaItem($mp_items[$mp_i-1],$opt) : __('(none)')).
-							'</div>';
+                for ($mp_i = 0; $mp_i < count($mp_items); $mp_i++) {
+                    if ($mp_items[$mp_i]->media_id == $file->media_id) {
+                        // On a trouvé le média courant dans la liste
 
-						// Image suivante
-						echo '<div class="media-item media-col-1">'.'<h4>'.__('Next media:').'</h4>'.
-							($mp_i < count($mp_items)-1 ? self::displayMediaItem($mp_items[$mp_i+1],$opt) : __('(none)')).
-							'</div>';
+                        // Média précédent
+                        echo '<div class="media-item media-col-0">' . '<h4>' . __('Previous media:') . '</h4>' .
+                            ($mp_i > 0 ? self::displayMediaItem($mp_items[$mp_i - 1], $opt) : __('(none)')) .
+                            '</div>';
 
-						break;
-					}
-				}
-				echo '</div></div>';
-			}
-		}
-	}
+                        // Image suivante
+                        echo '<div class="media-item media-col-1">' . '<h4>' . __('Next media:') . '</h4>' .
+                            ($mp_i < count($mp_items) - 1 ? self::displayMediaItem($mp_items[$mp_i + 1], $opt) : __('(none)')) .
+                            '</div>';
 
-	/**
-	Display media attributes and links
+                        break;
+                    }
+                }
+                echo '</div></div>';
+            }
+        }
+    }
 
-	@param	file	<b>fileItem</b>	File item
-	@param	opt	<b>string</b>	additional parameter for URL
-	*/
-	private static function displayMediaItem($file,$opt)
-	{
-		// Construction de l'URL pour le lien de navigation
-		$mp_link = 'media_item.php?id='.$file->media_id.$opt;
+    /**
+    Display media attributes and links
 
-		$ret =
+    @param    file    <b>fileItem</b>    File item
+    @param    opt    <b>string</b>    additional parameter for URL
+     */
+    private static function displayMediaItem($file, $opt)
+    {
+        // Construction de l'URL pour le lien de navigation
+        $mp_link = 'media_item.php?id=' . $file->media_id . $opt;
 
-		// Vignette du média avec lien de navigation
-		'<p><a class="media-icon media-link" href="'.$mp_link.'">'.
-		'<img style="margin-right: 0.5em; padding: 2px;" src="'.$file->media_icon.'" alt="" /></a></p>'.
+        $ret =
 
-		// Attributs
-		'<ul style="padding-bottom: 1em;">'.
+        // Vignette du média avec lien de navigation
+        '<p><a class="media-icon media-link" href="' . $mp_link . '">' .
+        '<img style="margin-right: 0.5em; padding: 2px;" src="' . $file->media_icon . '" alt="" /></a></p>' .
 
-			// Nom du fichier avec lien de navigation
-			'<li style="list-style: none outside none;"><a class="media-link" href="'.$mp_link.'">'.$file->basename.'</a></li>'.
+        // Attributs
+        '<ul style="padding-bottom: 1em;">' .
 
-			// Titre du média
-			'<li style="list-style: none outside none;">'.$file->media_title.'</li>'.
+        // Nom du fichier avec lien de navigation
+        '<li style="list-style: none outside none;"><a class="media-link" href="' . $mp_link . '">' . $file->basename . '</a></li>' .
 
-			// Date et taille du média et URL d'ouverture
-			'<li style="list-style: none outside none;">'.$file->media_dtstr.' - '.
-			files::size($file->size).' - '.'<a href="'.$file->file_url.'">'.__('open').'</a>'.
-			'</li>'.
+        // Titre du média
+        '<li style="list-style: none outside none;">' . $file->media_title . '</li>' .
 
-		'</ul>';
+        // Date et taille du média et URL d'ouverture
+        '<li style="list-style: none outside none;">' . $file->media_dtstr . ' - ' .
+        files::size($file->size) . ' - ' . '<a href="' . $file->file_url . '">' . __('open') . '</a>' .
+            '</li>' .
 
-		return $ret;
-	}
+            '</ul>';
+
+        return $ret;
+    }
 }
--- _define.php
+++ _define.php
@@ -1,25 +1,26 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Media Navigator, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief navmedia, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */				"Media Navigator",
-	/* Description*/		"Navigate between media in folder",
-	/* Author */			"Franck Paul",
-	/* Version */			'1.2',
-	array(
-		/* Permissions */	'permissions' =>	'contentadmin',
-		/* Type */			'type' =>			'plugin',
-		/* Priority */		'priority' => 		10000
-	)
+    "Media Navigator",                  // Name
+    "Navigate between media in folder", // Description
+    "Franck Paul",                      // Author
+    '1.2',                              // Version
+    array(
+        'permissions' => 'contentadmin', // Permissions
+        'type'        => 'plugin',       // Type
+        'priority'    => 10000          // Priority
+    )
 );
