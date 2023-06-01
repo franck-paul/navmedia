5ffd4fd47978684a8517f98b2e0b52c9b1279e6c
franck <carnet.franck.paul@gmail.com>
2023-06-01 10:37:21 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -14,6 +14,6 @@ use Dotclear\Helper\L10n;
 L10n::$locales['Media Navigator'] = 'Navigateur de média';
 L10n::$locales['Navigate between media in folder'] = 'Naviguer parmi les médias du répertoire';
 L10n::$locales['Previous media:'] = 'Média précédent :';
-L10n::$locales['(none)'] = '';
+L10n::$locales['(none)'] = '(aucun)';
 L10n::$locales['Next media:'] = 'Média suivant :';
-L10n::$locales['open'] = '';
+L10n::$locales['open'] = 'ouvrir';
