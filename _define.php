25c726acb796dff6e5de37376f01f87bfcf95a09
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:10:18 +0200

    New release 5.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Media Navigator',
     'Navigate between media in folder',
     'Franck Paul',
-    '4.0',
+    '5.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
